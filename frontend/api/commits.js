7b4d3978e1f19d9924c46c76535864b184cda674
Mattias <gillm7623@gmail.com>
2026-06-05 12:23:28 +0200

    shared folder for api and frontend for vercel

---
+++ frontend/api/commits.js
@@ -0,0 +1,35 @@
+import { getRecentCommits } from "./services/fetchGithub.js";
+
+let cache = null;
+let cacheTime = 0;
+
+const CACHE_DURATION = 120000;
+
+async function handler(req, res) {
+	try {
+		const now = Date.now();
+
+		if (cache && now - cacheTime < CACHE_DURATION) {
+			return res.status(200).json(cache);
+		}
+
+		const result = await getRecentCommits();
+
+		const repositories = result.data.viewer.repositories.nodes;
+
+		const commitsRepo = repositories.map((repo) => {
+			return repo.defaultBranchRef.target.history.nodes;
+		});
+
+		const commits = commitsRepo.flat();
+
+		cache = commits;
+		cacheTime = now;
+
+		return res.status(200).json(commits);
+	} catch (err) {
+		return res.status(500).json({ error: err.message });
+	}
+}
+
+export default handler;
